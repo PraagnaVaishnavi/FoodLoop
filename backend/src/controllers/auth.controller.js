6255ac9c1909e197ccdc41c95d98e3c9dbe781ba
hitanshi-singh <tech.hitanshi@gmail.com>
2025-04-01 04:40:21 +0530

    fix: response issue

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,35 +2,36 @@ import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 export const signup = async (req, res) => {
-    try {
-        const { name, email, password, role } = req.body;
+  try {
+    const { name, email, password, role } = req.body;
 
-        // Check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: "User already exists. Please log in." });
-        }
-
-        // Create and save new user
-        const user = new User({
-            name,
-            email,
-            password,
-            googleId: "g2", // fixing needed : googleID needs to be unique
-            role,
-        });
-
-        await user.save();
-        console.log("User saved successfully:", user);
-        res.status(201).json({ message: "User registered successfully" });
-
-    } catch (error) {
-        console.error("Signup Error:", error);
-        res.status(500).json({ error: "Signup failed", details: error.message });
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "User already exists. Please log in." });
     }
-};
 
-  
+    // Create and save new user
+    const user = new User({
+      name,
+      email,
+      password,
+      googleId: `${Math.random()}abc`, // fixing needed : googleID needs to be unique
+      role,
+    });
+
+    await user.save();
+    console.log("User saved successfully:", user);
+    res
+      .status(201)
+      .json({ message: "User registered successfully", success: true });
+  } catch (error) {
+    console.error("Signup Error:", error);
+    res.status(500).json({ error: "Signup failed", details: error.message });
+  }
+};
 
 export const login = async (req, res) => {
   try {
@@ -46,7 +47,7 @@ export const login = async (req, res) => {
     );
     res.json({ token, user });
   } catch (error) {
-    console.error("Login error:", error);  
+    console.error("Login error:", error);
     res.status(500).json({ error: "Login failed", details: error.message });
   }
 };
