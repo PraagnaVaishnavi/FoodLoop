c804c9e539316dcde15cdb4210f80f5454c14333
Sujith <sujithkb24@gmail.com>
2025-04-08 18:47:41 +0530

    Landing page added

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -3,12 +3,14 @@ import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Dashboard from "../pages/Dashboard";
 import ProtectedRoute from "./ProtectedRoute.jsx";
+import Landingpage from "../pages/Landingpage.jsx";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Landingpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route
           path="/dashboard"
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        merriweather: ["Merriweather", "serif"],
+        rouge: ["Rouge Script", "cursive"],
+        Birthstone: ["Birthstone", "serif"],
+      
+      },
+      colors: {
+        colour1: '#FFA725', 
+        colour2: '#FFF5E4', 
+        colour3: '#C1D8C3', 
+        colour4: '#6A9C89',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'scroll-left': 'scroll 30s linear infinite',
+      },
+    },
+
   },
   plugins: [],
-}
+};
