d31158928f827f31f8aab9c196c3a9538d3a2e36
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-04-23 01:06:41 +0530

    Added app loader

--- foodloop_mobile/lib/features/donations/services/impact_statistics.dart
+++ foodloop_mobile/lib/features/donations/services/impact_statistics.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class ImpactService {
   
   Future<Map<String, dynamic>> getImpactStats() async {
-    final response = await http.get(Uri.parse(ApiConstants.impactUrl));
+    final response = await http.get(Uri.parse(ApiConstants.impactStatistics));
     
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
