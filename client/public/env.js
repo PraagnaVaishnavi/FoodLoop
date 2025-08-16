4bdd148396154f6641a96769400d84ab7f73c969
rithesh-n <ritheshan18@gmail.com>
2025-08-16 11:58:42 +0530

    changes to the backendapi url for the production

--- client/public/env.js
+++ client/public/env.js
@@ -1,4 +1,5 @@
-window.ENV = {
+  window.ENV = {
     // BACKEND_API: "http://localhost:8000"
-    BACKEND_API: "https://foodloop-6bl4.onrender.com"
+    // BACKEND_API: "https://foodloop-6bl4.onrender.com"
+    BACKEND_API: "https://foodloop-72do.onrender.com"
   };
